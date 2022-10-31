9958c7e9073590185be5c40704050f1919200ed2
Zuza Zuber <zuza@lewagon.org>
2022-10-31 17:57:30 +0100

    use EIP712

--- src/helpers/client.ts
+++
@@ -1,7 +0,0 @@
-import Client from '@snapshot-labs/snapshot.js/src/client';
-
-const webhookUrl =
-  import.meta.env.SNAPSHOT_WEBHOOK_API || 'http://localhost:3000';
-const client = new Client(webhookUrl);
-
-export default client;
